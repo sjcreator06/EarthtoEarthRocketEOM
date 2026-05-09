4455cda10589cf22902459b2b753d150e6ade863
Samuel Joseph <73075829+sjcreator06@users.noreply.github.com>
2026-05-08 22:19:44 -0700

    Update SpaceX Starship_EarthtoEarth_Sims.m

--- SpaceX Starship_EarthtoEarth_Sims.m
+++ SpaceX Starship_EarthtoEarth_Sims.m
@@ -8,7 +8,8 @@
 
 %% Constants and Variables
 m_r = 5270000;                   % Total Mass of Rocket (kg)
-g = 9.81;                        % Gravitational Constant (m/s^2) 
+g = 9.81;                        % Gravitational Constant (m/s^2)
+r_earth = 6371000;               % Radius of Earth (m)
 L = 55.74;                       % Distance from Base of Rocket to Center of Mass (m)
 c_p = 55.09;                     % Location of Center of Pressure (m)
 CD = 0.6;                        % Coefficient of Drag
@@ -53,16 +54,67 @@ Ixx = I(1,1);
 Iyy = I(2,2);
 Izz = I(3,3);
 
-%% Atmospheric Model 
+%% Atmospheric Model for Density 
 
+% Altitude in terms of Cartesian Coordinates
+h = (x^2 + y^2 + z^2)^0.5 - r_earth;
 
+function [rho] = atmosphere(h)
+% Standard Sea Level Values
+rhoSL = 1.225;       % kg/m^3
+pSL   = 101325;      % Pa
+tSL   = 288.15;      % K
+R     = 287.058;     % J/(kg*K)
+gamma = 1.4;         
+
+% Variables
+% theta = Temperature Ratio
+% sigma = Pressure Ratio
+% T = Temperature
+% p = Pressure
+% rho = Density
+% a = Speed of Sound
+
+if h > 0 && h <= 36089 
+   theta = (1-6.875*10^(-6)*h);
+   T = theta*tSL;
+   sigma = (1-6.875*10^(-6)*h)^5.2561;
+   p = sigma*pSL;
+   rho = p/(R*T);
+
+elseif h > 36089 && h <= 65617 
+   theta = 0.75189;
+   T = theta*tSL;
+   sigma = 0.2234*exp((4.806*10^(-5)*(36089-h)));
+   p = sigma*pSL;
+   rho = p/(R*T);
+ 
+elseif h > 65617 && h <= 104990 
+   theta = 0.75189+1.0577*10^(-6)*(h-65617);
+   T = theta*tSL;
+   sigma = 3.174716*10^(-6)*(0.75189+(1.0577*10^-6)*(h-65617))^-34.164;
+   p = sigma*pSL;
+   rho = p/(R*T);
+end
+end
 
 %% Force Control
-
 % Thrust Force 
+F_t = 74400000;     % Constant Thrust Magnitude (Approximate Value)
 
-F_t = 10; % Thrust Magnitude 
+% Drag Force Components 
 
+% Velocity Squared Components 
+vx = (xdot*cos(phi)*cos(theta) + ydot*sin(phi)*cos(theta) + zdot*sin(theta) + L*thetadot)^2;
+vy = (ydot*cos(phi)-psidot*L*sin(theta))^2;
+vz = (xdot*sin(phi)*sin(theta)+zdot*cos(theta))^2;
+
+% Unit Vector Components 
+v_mag = (vx^2 + vy^2 + vz^2)^0.5;
+
+v1 = vx / v_mag;
+v2 = vy / v_mag;
+v3 = vz / v_mag;
 
 %% Equations of Motion
 
@@ -79,3 +131,6 @@ F = [-m_r*g*sin(theta) - F_t*sin(lambda)*cos(delta) - D_mag*v1 - m_r*L*psidot^2*
      -m_r*g*cos(theta) + F_t*cos(lambda) - D_mag*v3 + m_r*L*thetadot^2 + m_r*L*psidot^2*(sin(theta))^2;
             -L*F_t*sin(lambda)*sin(delta) - (L-c_p)*D_mag*v2 - Izz*thetadot*psidot*cos(theta);
         L*F_t*sin(lambda)*cos(delta) + (L-c_p)*D_mag*v1 -(Ixx-Izz)*psidot^2*sin(theta)*cos(theta)];
+
+% Acceleration State Vector 2nd Order ODE
+qdotdot = inv(M) * F;
